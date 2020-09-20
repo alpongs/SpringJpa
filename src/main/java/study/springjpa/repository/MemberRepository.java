8ccd5359e100e59eea361e7653b97150467c66f1
I. YOON <tingcobell@gmail.com>
2020-09-20 22:17:42 +0900

    Create Member Entity Repository.

--- src/main/java/study/springjpa/repository/MemberRepository.java
+++ src/main/java/study/springjpa/repository/MemberRepository.java
@@ -2,7 +2,12 @@ package study.springjpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 import study.springjpa.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findByName(String name);
+
 }
