a09c1061493875e6d35629da0a2d71b5ec82caf9
I. YOON <tingcobell@gmail.com>
2020-09-21 00:29:45 +0900

    Configuration 임시로 제외처리.

--- src/main/java/study/springjpa/config/AppConfig.java
+++ src/main/java/study/springjpa/config/AppConfig.java
@@ -1,10 +1,9 @@
 package study.springjpa.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "study.springjpa.repository")
+//@EnableJpaRepositories(basePackages = "study.springjpa.repository")
 public class AppConfig {
 
 }
