2f26cbc5091671683640223ee80351f92126fe0c
I. YOON <tingcobell@gmail.com>
2020-09-29 19:17:56 +0900

    Getter 추가.

--- src/main/java/study/springjpa/model/BaseEntity.java
+++ src/main/java/study/springjpa/model/BaseEntity.java
@@ -1,5 +1,6 @@
 package study.springjpa.model;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -7,8 +8,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-
-import lombok.Getter;
 
 
 @EntityListeners(AuditingEntityListener.class)
